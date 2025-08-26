63e6fcdd7686fba36bffe0970de31042cb5059dd
Ashik756 <ashikurrahaman9988@gmail.com>
2025-08-26 12:24:42 +0600

    problem fixed

---
+++ script/home.js
@@ -0,0 +1,11 @@
+function getEBID(id){
+    
+}
+
+document.getElementById("btn-addMoney").addEventListener("click", function () {
+    const balance = parseInt(document.getElementById("balance").innerText);
+    const addBalance = parseInt(document.getElementById("inp-money").value);
+    const newBalance = balance + addBalance;
+    document.getElementById("balance").innerText = newBalance;
+    document.getElementById("inp-money").value = "";
+});
